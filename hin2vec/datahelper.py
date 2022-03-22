9684eac24c2d56df93b0c9b5daf5e4913f1c3895
Sprinkhaan42 <branco.francois@student.kuleuven.be>
2022-03-22 18:31:30 +0100

    Data sampling pointed 2

--- hin2vec/datahelper.py
+++ hin2vec/datahelper.py
@@ -3,4 +3,5 @@ df = pd.read_csv('youshu_edges.csv')
 df1 = df.sample(frac =.01)
 print(len(df1))
 print(df1)
+#here I sampled for hin2vec and it did help.
 df1.to_csv("youshu_edges_sample.csv", index=False, sep=',')
